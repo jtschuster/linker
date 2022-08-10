41348d86dd6f1baf4bd8e7d958e63fe3a5236479
Jackson Schuster <jschuster@microsoft.com>
2022-08-10 13:17:40 -0700

    Add ILVerifier.cs and run in tests

--- test/Mono.Linker.Tests/TestCasesRunner/ILVerifier.cs
+++ test/Mono.Linker.Tests/TestCasesRunner/ILVerifier.cs
@@ -7,180 +7,137 @@ using System.Reflection;
 using System.Reflection.PortableExecutable;
 using System.Reflection.Metadata;
 using Internal.TypeSystem.Ecma;
-using Mono.Linker.Tests.Extensions;
 using System.IO;
-using Mono.Linker.Tests.Cases.Libraries;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
+using Mono.Linker.Tests.Extensions;
 using NUnit.Framework;
+using System.Resources;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Loader;
+using System.Drawing;
 
+#nullable enable
 namespace Mono.Linker.Tests.TestCasesRunner
 {
 	class ILVerifier : ILVerify.IResolver
 	{
-		IResolver resolver;
 		Verifier _verifier;
 		string _assemblyName;
-		NPath _path;
-		static Assembly systemModule = typeof (object).Assembly;
-		public ILVerifier(LinkedTestCaseResult linkResult)
+		NPath _assemblyPath;
+		NPath _assemblyFolder;
+		NPath _frameworkFolder;
+		Dictionary<string, PEReader> _assemblyCache;
+		AssemblyLoadContext _alc;
+
+		public IEnumerable<VerificationResult> Results { get; private set; }
+
+		public ILVerifier (NPath assemblyPath)
 		{
-			_assemblyName = linkResult.OutputAssemblyPath.FileNameWithoutExtension;
-			_path = linkResult.OutputAssemblyPath;
-			resolver =  this;
-			_verifier = new ILVerify.Verifier (resolver, new ILVerify.VerifierOptions { SanityChecks = true });
-			_verifier.SetSystemModuleName (typeof (object).Assembly.GetName ());
-			AssemblyName a = new AssemblyName (linkResult.OutputAssemblyPath.FileNameWithoutExtension);
-			//VerifyAssembly (a, linkResult.OutputAssemblyPath);
+			_assemblyPath = assemblyPath;
+			_assemblyName = assemblyPath.FileNameWithoutExtension;
+			_assemblyFolder = assemblyPath.Parent;
+			_assemblyCache = new Dictionary<string, PEReader> ();
+			_frameworkFolder = typeof (object).Assembly.Location.ToNPath ().Parent;
+			_alc = new AssemblyLoadContext (_assemblyFolder.FileName);
+			LoadAssembly ("mscorlib");
+			LoadAssembly ("System.Private.CoreLib");
+			LoadAssemblyFromPath (_assemblyName, _assemblyPath);
 
+			_verifier = new ILVerify.Verifier (
+				this,
+				new ILVerify.VerifierOptions {
+					SanityChecks = true,
+					IncludeMetadataTokensInErrorMessages = true
+				});
+			_verifier.SetSystemModuleName (new AssemblyName ("mscorlib"));
 
+			var allResults = _verifier.Verify (Resolve (_assemblyName))
+				?? Enumerable.Empty<VerificationResult> ();
 
-			var verificationResults = _verifier.Verify (resolver.ResolveAssembly (a));
-			if (verificationResults.Count () != 0) {
-				var failureMessages = new StringBuilder ();
-				foreach (VerificationResult result in verificationResults) {
-					if (result.Code == ILVerify.VerifierError.None)
-						continue;
-					Assert.Fail ($"ILVerify error: {result.Message}, {result}");
-				}
+			Results = allResults.Where (r => r.Code switch {
+				ILVerify.VerifierError.None
+				// Static interface methods cause this warning
+				or ILVerify.VerifierError.CallAbstract
+				// "Missing callVirt after constrained prefix - static interface methods cause this warning
+				or ILVerify.VerifierError.Constrained
+				// ex. localloc cannot be statically verified by ILVerify
+				or ILVerify.VerifierError.Unverifiable
+				// ref returning a ref local causes this warning but is okay
+				or VerifierError.ReturnPtrToStack
+				// Span indexing with indexer (ex. span[^4]) causes this warning
+				or VerifierError.InitOnly
+				=> false,
+				_ => true
+			});
+		}
+
+		PEReader LoadAssembly (string assemblyName)
+		{
+			if (_assemblyCache.TryGetValue (assemblyName, out PEReader? reader))
+				return reader;
+			var assembly = _alc.LoadFromAssemblyName (new AssemblyName (assemblyName));
+			reader = new PEReader (File.OpenRead (assembly.Location));
+			_assemblyCache.Add (assemblyName, reader);
+			return reader;
+		}
+
+		PEReader LoadAssemblyFromPath (string assemblyName, NPath pathToAssembly)
+		{
+			if (_assemblyCache.TryGetValue (assemblyName, out PEReader? reader))
+				return reader;
+			var assembly = _alc.LoadFromAssemblyPath (pathToAssembly);
+			reader = new PEReader (File.OpenRead (assembly.Location));
+			_assemblyCache.Add (assemblyName, reader);
+			return reader;
+		}
+
+		bool TryLoadAssemblyFromFolder (string assemblyName, NPath folder, [NotNullWhen (true)] out PEReader? peReader)
+		{
+			Assembly? assembly = null;
+			string assemblyPath = Path.Join (folder.ToString (), assemblyName);
+			if (File.Exists (assemblyPath + ".dll"))
+				assembly = _alc.LoadFromAssemblyPath (assemblyPath + ".dll");
+			else if (File.Exists (assemblyPath + ".exe"))
+				assembly = _alc.LoadFromAssemblyPath (assemblyPath + ".exe");
+
+			if (assembly is not null) {
+				peReader = new PEReader (File.OpenRead (assembly.Location));
+				_assemblyCache.Add (assemblyName, peReader);
+				return true;
 			}
+			peReader = null;
+			return false;
 		}
 
-		public PEReader Resolve (string simpleName)
+		PEReader? Resolve (string assemblyName)
 		{
-			if (simpleName == systemModule.GetName ().Name)
-				return GetSystemModule ();
-			if (simpleName != _assemblyName)
-				return null;
+			PEReader? reader;
+			if (_assemblyCache.TryGetValue (assemblyName, out reader)) {
+				return reader;
+			}
 
-			PEReader result = new PEReader (File.OpenRead (_path));
-			return result;
+			if (TryLoadAssemblyFromFolder (assemblyName, _frameworkFolder, out reader))
+				return reader;
+
+			if (TryLoadAssemblyFromFolder (assemblyName, _assemblyFolder, out reader))
+				return reader;
+
+			return null;
 		}
 
-		static PEReader GetSystemModule () =>
-			new PEReader (File.OpenRead (systemModule.Location));
+		PEReader? ILVerify.IResolver.ResolveAssembly (AssemblyName assemblyName)
+			=> Resolve (assemblyName.Name ?? assemblyName.FullName);
 
-		PEReader ILVerify.IResolver.ResolveAssembly (AssemblyName assemblyName)
-			=> assemblyName.FullName == systemModule.FullName ? GetSystemModule () : Resolve (assemblyName.Name);
-
-		PEReader ILVerify.IResolver.ResolveModule (AssemblyName referencingModule, string fileName)
+		PEReader? ILVerify.IResolver.ResolveModule (AssemblyName referencingModule, string fileName)
 			=> Resolve (Path.GetFileNameWithoutExtension (fileName));
 
 
-		private string GetVerifyMethodsResult (VerificationResult result, EcmaModule module, string pathOrModuleName)
+		public string GetErrorMessage (VerificationResult result)
 		{
-			string message = "[IL]: Error [";
-			if (result.Code != ILVerify.VerifierError.None) {
-				message += result.Code;
-			} else {
-				message += result.ExceptionID;
-			}
-			message += "]: ";
-
-			message += "[";
-			message += pathOrModuleName;
-			message += " : ";
-
-			MetadataReader metadataReader = module.MetadataReader;
-
-			TypeDefinition typeDef = metadataReader.GetTypeDefinition (metadataReader.GetMethodDefinition (result.Method).GetDeclaringType ());
-			string typeNamespace = metadataReader.GetString (typeDef.Namespace);
-			message += typeNamespace;
-			message += ".";
-			string typeName = metadataReader.GetString (typeDef.Name);
-			message += typeName;
-
-			message += "::";
-			var method = (EcmaMethod) module.GetMethod (result.Method);
-			message += PrintMethod (method);
-			message += "]";
-
-			if (result.Code != VerifierError.None) {
-				message += "[offset 0x";
-				message += result.GetArgumentValue<int> ("Offset").ToString ("X8");
-				message += "]";
-
-				if (result.TryGetArgumentValue ("Found", out string found)) {
-					message += "[found ";
-					message += found;
-					message += "]";
-				}
-
-				if (result.TryGetArgumentValue ("Expected", out string expected)) {
-					message += "[expected ";
-					message += expected;
-					message += "]";
-				}
-
-				if (result.TryGetArgumentValue ("Token", out int token)) {
-					message += "[token  0x";
-					message += token.ToString ("X8");
-					message += "]";
-				}
-			}
-
-			message += " ";
-			message += result.Message + System.Environment.NewLine;
-			return message;
+			return $"IL Verification error:\n{result.Message}";
 		}
-		private static string PrintMethod (EcmaMethod method)
-		{
-			string displayName = method.Name;
-			displayName += "(";
-			try {
-				if (method.Signature.Length > 0) {
-					bool first = true;
-					for (int i = 0; i < method.Signature.Length; i++) {
-						Internal.TypeSystem.TypeDesc parameter = method.Signature[i];
-						if (first) {
-							first = false;
-						} else {
-							displayName += ", ";
-						}
-
-						displayName += parameter.ToString ();
-					}
-				}
-			} catch {
-				displayName = "Error while getting method signature";
-			}
-			displayName += ")";
-			return displayName;
-		}
-		//private int VerifyAssembly(AssemblyName name, string path)
-  //      {
-  //          PEReader peReader = Resolve(name.Name);
-  //          EcmaModule module = _verifier.GetModule);
-
-  //          return VerifyAssembly(peReader, module, path);
-  //      }
-
-  //      private int VerifyAssembly(PEReader peReader, EcmaModule module, string path)
-  //      {
-  //          int numErrors = 0;
-  //          int verifiedMethodCounter = 0;
-  //          int methodCounter = 0;
-  //          int verifiedTypeCounter = 0;
-  //          int typeCounter = 0;
-
-  //          VerifyMethods(peReader, module, path, ref numErrors, ref verifiedMethodCounter, ref methodCounter);
-  //          VerifyTypes(peReader, module, path, ref numErrors, ref verifiedTypeCounter, ref typeCounter);
-
-  //          if (numErrors > 0)
-  //              WriteLine(numErrors + " Error(s) Verifying " + path);
-  //          else
-  //              WriteLine("All Classes and Methods in " + path + " Verified.");
-
-  //          if (_options.Statistics)
-  //          {
-  //              WriteLine($"Types found: {typeCounter}");
-  //              WriteLine($"Types verified: {verifiedTypeCounter}");
-
-  //              WriteLine($"Methods found: {methodCounter}");
-  //              WriteLine($"Methods verified: {verifiedMethodCounter}");
-  //          }
-
-  //          return numErrors;
-  //      }
 	}
 }
+#nullable restore
